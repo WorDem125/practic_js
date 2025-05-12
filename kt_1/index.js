82aaf6068f4301a36ddf66cc66cf2a5d7a37dafa
WorDem125 <const.reg@yandex.ru>
2025-05-12 22:52:03 +1000

    Update index.js

--- kt_1/index.js
+++ kt_1/index.js
@@ -16,3 +16,95 @@ if (range === "month") {
 // Формируем строку
 let result = `${price} Р в ${rangeInRussian}`;
 console.log(result); // Вывод результата
+
+
+
+//////////////////////////////////////////////////////
+
+
+let temp = 25; // Температура
+let weather = "clear"; // Погода
+
+// Объявляем переменную activity
+let activity;
+
+// Используем условия для определения значения activity
+if (temp >= 25 && weather === "clear") {
+  activity = "golf"; // Если тепло и ясно
+} else if ((temp >= 10 && temp <= 24) || weather === "cloudy") {
+  activity = "bowling"; // Если температура от 10 до 24 или облачно
+} else {
+  activity = "hiking"; // Во всех остальных случаях
+}
+
+// Выводим результат
+console.log(activity);
+
+
+//////////////////////////////////////////////////////
+
+
+let a = 2; // Первое число
+let b = 6; // Второе число
+let sign = "*"; // Оператор
+
+// Используем конструкцию if...else для проверки оператора
+if (sign === "+") {
+  console.log(a + b); // Сложение
+} else if (sign === "-") {
+  console.log(a - b); // Вычитание
+} else if (sign === "*") {
+  console.log(a * b); // Умножение
+} else if (sign === "/") {
+  console.log(a / b); // Деление
+} else {
+  console.log("Неверный оператор"); // Если знак не распознан
+}
+
+
+//////////////////////////////////////////////////////
+
+
+let a = 2; // Первое число
+let b = 0; // Второе число
+let sign = "/"; // Оператор
+
+// Используем конструкцию if...else для проверки оператора
+if (sign === "+") {
+  console.log(a + b); // Сложение
+} else if (sign === "-") {
+  console.log(a - b); // Вычитание
+} else if (sign === "*") {
+  console.log(a * b); // Умножение
+} else if (sign === "/") {
+  if (b === 0) {
+    console.log("Делить на 0 нельзя!"); // Проверка на деление на 0
+  } else {
+    console.log(a / b); // Деление
+  }
+} else {
+  console.log("Неверный оператор"); // Если знак не распознан
+}
+
+
+//////////////////////////////////////////////////////
+
+
+let word = 'меню'; // Задаем строковую переменную word
+let length = word.length; // Находим длину строки word
+let sqrtLength = Math.sqrt(length); // Вычисляем квадратный корень длины
+let result; // Объявляем переменную result
+
+if (Number.isInteger(sqrtLength)) {
+  result = 1; // Если квадратный корень целое число
+} else {
+  result = 0; // Если квадратный корень дробное число
+}
+
+console.log(result); // Выводим результат
+
+
+
+
+
+
